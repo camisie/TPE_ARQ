ae54e85bb6d7e940cd3ad09109a1eedc21b5b27b
magflores <magflores@itba.edu.ar>
2021-11-09 22:41:46 -0300

    PokeColor

--- Userland/SampleCodeModule/include/pokemon.h
+++ Userland/SampleCodeModule/include/pokemon.h
@@ -4,9 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 
-//Imprime el logo de pokemon
-void printLogo(void);
-
 //Dependiendo del nro seleccionado, imprime el pokemon que encuentra cuando gana el juego
 void iChooseYou(int num);
 
